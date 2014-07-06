366acf50b0c826c89344cd2da0f0a2e665f941be
Sean Mee <sean.mee@hotmail.com>
2014-07-05 22:49:53 -0400

    fixed linkage between clients and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,14 @@
 class Project < ActiveRecord::Base
-  belongs_to :comics
+  belongs_to :comic
+  validates :comic_id, presence: true
+
+  before_validation :integize
 
   def comic
   end
+
+  def integize
+  	puts self.comic_id 
+  end
+
 end
